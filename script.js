1928d8df7361dff2b18275bead8d379766b09895
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-05-07 23:46:02 +0200

    Add adding books functionality

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const myLibrary = [];
-const content = document.querySelector("content");
-const addBookButton = document.getElementById("add_book");
+const content = document.querySelector('content');
+const newBookButton = document.getElementById('new_book');
+const addBookButton = document.getElementById('add_book_button');
+const addBookFormScreen = document.getElementById('add_book_form_screen');
+const addBookForm = document.getElementById('add_book_form');
+const closeAddBookFormButton = document.getElementById('exit_form');
 
-function showAddBookForm() {}
+function showAddBookForm() {
+  addBookFormScreen.style.visibility = 'visible';
+}
 
-addBookButton.addEventListener("click", () => {
+function hideAddBookForm() {
+  addBookFormScreen.style.visibility = 'hidden';
+}
+
+newBookButton.addEventListener('click', () => {
   showAddBookForm();
 });
 
-function Book(title) {
-  this.title = title;
+closeAddBookFormButton.addEventListener('click', () => {
+  hideAddBookForm();
+});
+
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+class Book {
+  constructor(title, author = 'undefined', pages = 'undefined') {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages) {
+  myLibrary.push(new Book(title, author, pages));
+}
+
+addBookButton.addEventListener('click', () => {
+  const bookTitleField = document.getElementById('book_title');
+  const bookAuthorField = document.getElementById('author');
+  const bookPagesField = document.getElementById('pages');
+  addBookToLibrary(bookTitleField.value, bookAuthorField.value, bookPagesField.value);
+});
